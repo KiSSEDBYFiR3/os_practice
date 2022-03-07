3e175f4d4af1a4a583bd9726f8285ed12a899a8c
Stanislav Moiseev <72256017+KiSSEDBYFiR3@users.noreply.github.com>
2022-03-07 19:45:47 +0300

    Delete first.c

--- 1/1/first.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> 
-#include <sys/types.h> 
-#include <unistd.h> 
-int main() 
-{ 
-    printf("PID текущего процесса is: %d\nPID родительского процесса: %d\n", getpid(), getppid());
-    return 0; 
-}
